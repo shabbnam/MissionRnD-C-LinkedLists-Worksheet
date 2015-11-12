2b15a8e5d94868bcf20500bb2fe06191e0fcc192
shabbnam <shabrnd@gmail.com>
2015-11-13 02:29:55 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -12,12 +12,53 @@ NOTES: Without using extra array or linked list.
 */
 
 #include <stdio.h>
-
+void sort(struct node *head);
+void checkmin(struct node *p);
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * sortLinkedList(struct node *head) {
+	if (head == NULL)return NULL;
+	else
+	{
+    	sort(head);
+		return head;
+	}
 	return NULL;
+}
+void checkmin(struct node *p)
+{
+	int  min = p->num, temp;
+	struct node *save = p, *ptr = p;
+	for (p = p->next; p != NULL; p = p->next)
+	{
+		if (p->num<min)
+		{
+			min = p->num;
+			save = p;
+		}
+
+	}
+
+	temp = save->num;
+	save->num = ptr->num;
+	ptr->num = temp;
+
+
+}
+
+
+void sort(struct node *head)
+{
+
+	struct node *p = head;
+	while (p->next != NULL)
+	{
+		checkmin(p);
+		p = p->next;
+	}
+	
+
 }
